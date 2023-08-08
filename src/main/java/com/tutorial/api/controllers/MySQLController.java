cf43428b76511f6de993dffd8c7cbda4d99a87c8
preetamshandilya <hbppreetamsingh850@gmail.com>
2023-08-08 22:18:20 +0530

    changed name to MySQLController

--- src/main/java/com/tutorial/api/controllers/MySQLController.java
+++ src/main/java/com/tutorial/api/controllers/MySQLController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MySQLTestController {
+public class MySQLController {
 
     @Autowired
     public MySQLTestService mySQLTestService;
